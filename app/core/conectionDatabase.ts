620cde6f604e4264f0e09d2f4bdb248157504b01
brasilino <jefferson.wendhel@gmail.com>
2019-07-16 22:16:48 -0300

    .gitignore

--- app/core/conectionDatabase.ts
+++ app/core/conectionDatabase.ts
@@ -1,23 +1,20 @@
 import {Sequelize} from 'sequelize';
 import * as dotenv from 'dotenv';
-import * as t from 'local-storage';
 import {databases} from '../config/databases';
 
 class ConectionDataBase {
     public conection: any;
+    private databases: Object;
+
+    constructor() {
+        this.databases = databases;
+    }
 
     conect = (): Sequelize => {
         dotenv.config();
         let schemaSelected = String(process.env.DB_HOST_DEV);
-        console.log('schema selected ->', databases[schemaSelected]);
         try {
-            this.conection = new Sequelize(Object(databases[schemaSelected]));
-            this.conection.authenticate()
-                .then(() => {
-                    console.log('conexão com o banco bem sucedida.');
-                }).catch((err) => {
-                console.error('conexão falhou', err);
-            });
+            this.conection = new Sequelize(Object(this.databases[schemaSelected]));
         } catch (err) {
             console.log('erro ao conectar com o banco de dados');
         }
@@ -31,15 +28,34 @@ class ConectionDataBase {
     * de conexão ser chamada.
     * */
     changeDatabase = async (database) => {
-        //remove o cabeçalho de conexao, evitando ter fila de cabeçalhos de conexao;
-        await this.conection.removeHook('beforeConnect','changeConnection');
+        try {
+            if (this.databases[database]) {
+                //remove o cabeçalho de conexao, evitando ter fila de cabeçalhos de conexao;
+                await this.conection.removeHook('beforeConnect', 'changeConnection');
 
-        //adiciona o hoook e o executa fazendo a mudança de conexão;
-        await this.conection.beforeConnect('changeConnection', (config) => {
-            let dbConfig = databases[database];
-            for (let conf in dbConfig) {
-                config[conf] = dbConfig[conf];
+                //adiciona o hoook e o executa fazendo a mudança de conexão;
+                await this.conection.beforeConnect('changeConnection', (config) => {
+                    let dbConfig = databases[database];
+                    for (let conf in dbConfig) {
+                        config[conf] = dbConfig[conf];
+                    }
+                });
+                return true;
+            } else {
+                return false;
             }
+        } catch (e) {
+            console.log('ocorreu um erro ao mudar de conexao');
+            return false;
+        }
+    }
+
+    testConnection = () => {
+        this.conection.authenticate()
+            .then(() => {
+                console.log('conexão com o banco bem sucedida.');
+            }).catch((err) => {
+            console.error('conexão falhou', err);
         });
     }
 }
